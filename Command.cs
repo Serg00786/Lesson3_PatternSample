88c7e20613e34e59b6a216dc030f5ea4bfe8ac02
Sergey <s.alekseev@hevelsolar.com>
2021-10-26 13:16:55 +0300

    First commit

---
+++ Command.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace PatternSample
+{
+    public class Command : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+        private Action<object> execute;
+        private Func<object, bool> canExecute;
+
+        public Command(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+        
+
+        public bool CanExecute(object parameter)
+        {
+            return this.canExecute == null || this.canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            this.execute(parameter);
+        }
+    }
+
+
+
+        class WebService : Service
+    {
+        public WebService(string n) : base(n) { }
+        public override Command Create()
+        {
+            var cmd = new Command(o => { MessageBox.Show(Name + o.ToString()); });
+            cmd.Execute("1");
+            return cmd;
+            
+        }
+    }
+
+    class Desktop : Service
+    {
+        
+        public Desktop(string n) : base(n) { }
+        public override Command Create()
+        {
+            var cmd = new Command(o => { MessageBox.Show(Name + o.ToString()); });
+            cmd.Execute("1");
+            return cmd;
+        }
+    }
+
+    abstract class Service
+    {
+        public Service (string _name)
+        {
+            Name = _name;
+        }
+        public string Name { get; set; }
+        abstract public Command Create();
+    }
+
+
+}
